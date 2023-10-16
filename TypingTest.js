9a3dece410d9a3ca65647a219f2f2fee531cb3e4
Horseface <prestegaard9@gmail.com>
2023-10-17 00:19:21 +0200

    fix

--- TypingTest.js
+++ TypingTest.js
@@ -1,22 +1,16 @@
 let vTarget = document.getElementsByClassName("letters")[0];
-let vKeyEvents = [];
-function run(){
-    vText = vTarget.innerText
-    for (let index = 0; index < vText.length; index++) {
-        vKeyEvents.push(
-            new KeyboardEvent('keydown', 
-                {
-                    key:vText[index], 
-                    isComposing: true, 
-                    view:window,
-                    bubbles: true
-                }
-            )
-        );
-    }
-
-    for (let index = 0; index < vKeyEvents.length; index++) {
-        vTarget.dispatchEvent(vKeyEvents[index])
-    }
+vText = vTarget.innerText
+for (let index = 0; index < vText.length; index++) {
+    vTarget.dispatchEvent(
+        new KeyboardEvent('keydown', 
+            {
+                key:vText[index], 
+                isComposing: true, 
+                view:window,
+                bubbles: true
+            }
+        )
+    );
 }
-run()
+
+
